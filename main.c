d929fa434c7c9fcb2c6ee67994f55bf91ef2cd81
Denis Zaharov <aresepera@gmail.com>
2021-02-14 18:31:08 -0500

    added main file with unicode chess symbols

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[])
+{
+    // UNICODE CHESS SYMBOLS
+    char pawn[4] = "\u265F";
+    char knight[4] = "\u265E";
+    char bishop[4] = "\u265D";
+    char rook[4] = "\u265C";
+    char queen[4] = "\u265B";
+    char king[4] = "\u265A";
+
+    return 0;
+}
