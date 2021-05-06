9a45f480d9430c8919584b7d3fc4a0faf15b8a77
Denis Zaharov <aresepera@gmail.com>
2021-05-06 00:07:32 -0400

    Changed makefile and folder structure for unit testing

---
+++ test/main.c
@@ -0,0 +1,12 @@
+#define CTEST_MAIN
+
+#include <ctest.h>
+#include <libchessviz/chess.h>
+
+int main(int argc, const char** argv)
+{
+    return ctest_main(argc, argv);
+}
+
+CTEST(suite1, test1) {
+}
